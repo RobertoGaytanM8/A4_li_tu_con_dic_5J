a56980e1b30e9847bd56c19861611793829f5568
Roberto Gaytan Mendoza <a.22308051281202@cbtis128.edu.mx>
2024-09-11 07:46:46 -0600

    Add files via upload

---
+++ mi_diccionario.py
@@ -0,0 +1,14 @@
+print("Ejemplos de Diccionario")
+miperro = {
+  "Raza": "FrenchPoodle",
+  "nombre": "Terreneitor",
+  "edad": 3
+}
+print(miperro)
+
+print("Ciclo for")
+
+for gaytan in miperro:
+  print(gaytan)
+
+
