458c7983b40774ec828516f6a50e0e678af30503
Roberto Gaytan Mendoza <a.22308051281202@cbtis128.edu.mx>
2024-09-11 07:37:56 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo conjuntos set")
+tipodeportes={"futbol","Tenis","basquetball"}
+print(tipodeportes)
+print("Lista elementos del conjuntos tipodeportes loop for")
+print(tipodeportes)
+for gaytan in tipodeportes:
+    print(gaytan)
